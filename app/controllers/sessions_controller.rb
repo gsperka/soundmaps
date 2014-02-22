53cb8d1e700eb17f7f017cbef0e2844bc96f0395
lydiakoller1@gmail.com <lydiakoller1@gmail.com>
2014-02-22 14:59:49 -0600

    style buttons and form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:session][:username])
-    if user.authenticate(params[:session][:password])
-      session[:current_user_id] = user.id
+    @user = User.find_by(username: params[:session][:username])
+    if @user.authenticate(params[:session][:password])
+      session[:current_user_id] = @user.id
       redirect_to root_path
     else
-      redirect_to login_path
+      redirect_to root_path 
     end
   end
 
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,16 +1,4 @@
 require 'spec_helper'
 
-feature "user browsing the website" do
-  context "on homepage" do
-    it "can visit home page" do
-      visit root_path
-      expect(page).to have_content("SoundMap")
-    end
 
-    it "can click link to signup page" do
-      visit root_path
-      click_link "Sign Up"
-      expect(page).to have_content("Please Sign Up")
-    end
-  end
-end
+
