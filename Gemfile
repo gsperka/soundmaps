7d30c1d2742f1cf4843700053890bd55a19fd0ff
Apprentice <gsperka@gmail.com>
2014-02-21 20:01:39 -0600

    really fixed it this time

--- Gemfile
+++ Gemfile
@@ -60,4 +60,4 @@ gem 'jquery-turbolinks'
 # gem 'capistrano', group: :development
 
 # Use debugger
-# gem 'debugger', group: [:development, :test]
+gem 'debugger', group: [:development, :test]
--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -3,15 +3,11 @@ class TracksController < ActionController::Base
 
 	def create
 		@track = Track.new(track_params)
-		url = @track.clip.url.to_s
-		p url + "-----------------------------------------------------------"
-		@track.url = url
-
 		if @track.save
-			puts 'ASDLFKAJ$RLFIJF$'
+			@track.url = @track.clip.url.to_s
+			@track.save
 			 redirect_to root_path
 		else
-			puts 'failfailfailfail'
 			# render 'new'
 		end
 	end
